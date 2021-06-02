568d83c4a0d0ffa96433e03115ef72d887dbe62a
mailingliam02 <percy.bondy@gmail.com>
2021-06-02 19:58:32 +0200

    Added test file

--- build/main.bat
+++ build/main.bat
@@ -1,3 +1,3 @@
 @echo off
-python ..\src\clean_covid_dataset.py && python ..\src\svm.py && python ..\src\rf.py && python ..\src\nn.py && python ..\src\score.py
+python ..\test\tests.py && python ..\src\clean_covid_dataset.py && python ..\src\svm.py && python ..\src\rf.py && python ..\src\nn.py && python ..\src\score.py
 @pause
