c81c79f583377f773e298564c6d6e1820114ee6e
mailingliam02 <percy.bondy@gmail.com>
2021-06-02 20:20:18 +0200

    Fixing bug in src\__init__.py causing script to crash

--- build/main.bat
+++ build/main.bat
@@ -1,3 +1,3 @@
 @echo off
-python ..\test\tests.py && python ..\src\clean_covid_dataset.py && python ..\src\svm.py && python ..\src\rf.py && python ..\src\nn.py && python ..\src\score.py
+python ..\test\test_dataset_cleaner.py && python ..\src\clean_covid_dataset.py && python ..\src\svm.py && python ..\src\rf.py && python ..\src\nn.py && python ..\src\score.py
 @pause
